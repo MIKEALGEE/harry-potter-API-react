666e6fab9f66d89ba4524948b803806591835497
Michael Griffin <michaelgriffin121@gmail.com>
2018-12-12 22:55:37 +0000

    visual elements still need fixed

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import {Button, Card, CardBody, CardImage, CardTitle, CardText, Col} from 'mdbreact';
 import upperCaser from '../upperCaseHelper.js';
 
 const Character = ({name, house, image, wand}) =>{
+  const url = "http://harrypotter.wikia.com/wiki/Hogwarts_Houses#"+house
+  const imageUrl = "http://www.krismac.com/wp-content/uploads/2018/12/"+house+".jpg"
   return (
+      <Col>
+        <Card style={{ width: "22rem" }}>
 
-    <>
-      <h4>{name} {house}</h4>
-      <img src={image} height="240" alt="individual character headshot"/>
-      <h4>Wand: Wood: {upperCaser(wand.wood)} Core: {upperCaser(wand.core)}  Length: {wand.length}"</h4>
-    </>
+
+          <CardBody id={house}>
+            <CardTitle>{name}, {house}</CardTitle>
+              <img src={image} height="180" width="150"/>
+
+
+              <CardText>
+              Wand: Wood: {upperCaser(wand.wood)},  Core: {upperCaser(wand.core)}, Length: {upperCaser(wand.length)}"
+            </CardText>
+            <Button href= {url}>{house}</Button>
+          </CardBody>
+        </Card>
+      </Col>
   )
+
+
 
 }
 
